7d2472eb453a59fbfd401fa19a746991bdd737d7
pyjun01 <pyjun02@gmail.com>
2021-03-09 00:16:21 +0900

    Write query resolvers

--- server/src/schema.js
+++ server/src/schema.js
@@ -33,9 +33,22 @@ const typeDefs = gql`
   }
 
   type Query {
-    launches: [Launch]!
+    launches(
+      pageSize: Int
+      after: String
+    ): LaunchConnection!
     launch(id: ID!): Launch
     me: User
+  }
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
   }
 
   type TripUpdateResponse {
