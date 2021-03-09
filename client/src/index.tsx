18894c44e03c7597e97ffa795e0a0c742c621dd3
pyjun01 <pyjun02@gmail.com>
2021-03-10 01:47:18 +0900

    Fetch data with queries

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,26 +1,25 @@
 import {
   ApolloClient,
-  gql,
-  NormalizedCacheObject
+  NormalizedCacheObject,
+  ApolloProvider
 } from '@apollo/client';
 import { cache } from './cache';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+import injectStyles from './styles';
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   uri: 'http://localhost:4000/graphql'
 });
 
-client
-  .query({
-    query: gql`
-      query TestQuery {
-        launch(id: 56) {
-          id
-          mission {
-            name
-          }
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
+injectStyles();
+
+// Pass the ApolloClient instance to the ApolloProvider component
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
