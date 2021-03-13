e412ffed9a3e932bdd58d062193cf5607c937183
pyjun01 <pyjun02@gmail.com>
2021-03-13 23:46:46 +0900

    Manage local state

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,42 @@
 import {
   ApolloClient,
   NormalizedCacheObject,
-  ApolloProvider
+  ApolloProvider,
+  gql,
+  useQuery
 } from '@apollo/client';
+import Login from './pages/login'
 import { cache } from './cache';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
 import injectStyles from './styles';
 
+export const typeDefs = gql`
+  extend type Query {
+    isLoggedIn: Boolean!
+    cartItems: [ID!]!
+  }
+`;
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
-  uri: 'http://localhost:4000/graphql'
+  uri: 'http://localhost:4000/graphql',
+  headers: {
+    authorization: localStorage.getItem('token') || '',
+  },
+  typeDefs
 });
 
 injectStyles();
@@ -19,7 +44,7 @@ injectStyles();
 // Pass the ApolloClient instance to the ApolloProvider component
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById('root')
 );
